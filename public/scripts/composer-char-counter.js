a3638c602b05a224ce11db5a2777360f3a6d4e8f
Your Name <thomas.w.yee@gmail.com>
2021-01-26 16:19:27 -0500

    project setted up, and added simple functions like counting output length

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  // we are checking each key press inside the textarea
+  $('#tweet-text').on('input change keyup', function () {
+    let output = $(this).val();
+    // if this is no output yet, show 140
+    output.length === 0
+      ? $('.counter').text(`140`)
+      : //if the output length is smaller than 140, show output length
+      output.length <= 140
+      ? $('.counter').text(`${output.length}`)
+      : //if the output length is larger than 140, show 140-length in red
+        $('.counter')
+          .text(`${140 - output.length}`)
+          .css({ color: 'red' });
+  });
+});
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,13 +1,12 @@
-"use strict";
+'use strict';
 
-const userHelper    = require("../lib/util/user-helper")
+const userHelper = require('../lib/util/user-helper');
 
-const express       = require('express');
-const tweetsRoutes  = express.Router();
+const express = require('express');
+const tweetsRoutes = express.Router();
 
-module.exports = function(DataHelpers) {
-
-  tweetsRoutes.get("/", function(req, res) {
+module.exports = function (DataHelpers) {
+  tweetsRoutes.get('/', function (req, res) {
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -17,21 +16,22 @@ module.exports = function(DataHelpers) {
     });
   });
 
-  tweetsRoutes.post("/", function(req, res) {
+  tweetsRoutes.post('/', function (req, res) {
     if (!req.body.text) {
-      res.status(400).json({ error: 'invalid request: no data in POST body'});
+      res.status(400).json({ error: 'invalid request: no data in POST body' });
       return;
     }
-
-    const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
+    const user = req.body.user
+      ? req.body.user
+      : userHelper.generateRandomUser();
     const tweet = {
       user: user,
       content: {
-        text: req.body.text
+        text: req.body.text,
       },
-      created_at: Date.now()
+      created_at: Date.now(),
     };
-
+    console.log(user, tweet);
     DataHelpers.saveTweet(tweet, (err) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -42,5 +42,4 @@ module.exports = function(DataHelpers) {
   });
 
   return tweetsRoutes;
-
-}
+};

94f7b9ecb7e616c1a57cdad829ca50a139d6a3c9
Your Name <thomas.w.yee@gmail.com>
2021-01-26 19:24:49 -0500

    fixed nav_bar with addition of sticky class when window scrolled

---
+++ public/scripts/nav-sticky.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  //
+  $(window).scroll(function () {
+    //if you hard code, then use console
+    //.log to determine when you want the
+    //nav bar to stick.
+    console.log($(window).scrollTop());
+    if ($(window).scrollTop() > 10) {
+      $('#nav_bar').addClass('sticky');
+    }
+    if ($(window).scrollTop() < 10) {
+      $('#nav_bar').removeClass('sticky');
+    }
+  });
+});
